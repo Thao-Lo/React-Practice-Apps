132b269cc575fa05ed3aa77347f2b1efe4581888
Zavis <thezavis@gmail.com>
2025-02-05 01:52:58 +1100

    package-json install Mui, react router dom, apply it to Apps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import NotificationComponent from './components/NotificationComponent';
+import SendMessageComponent from './components/SendMessageComponent';
+import { WebSocketProvider } from './hooks/WebSocketContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <WebSocketProvider>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/send-notification" element={<SendMessageComponent />}/>
+        <Route path="/" element={ <NotificationComponent />}/>
+      </Routes>
+      </BrowserRouter>
+    </WebSocketProvider>
   );
 }
 
